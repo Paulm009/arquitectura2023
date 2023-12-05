b41a20511e4c67bc2484bb5f47b46fd2b5472402
Huascar Nina <huascarnina@gmail.com>
2023-12-05 19:18:28 -0400

    [UPDATE] Login y header componente actualizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarruselComponent } from './carrusel/carrusel.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+
 const routes: Routes = [
   { path: "carrusel", component: CarruselComponent },
   { path: "login", component: LoginComponent },
+  { path: "header", component: HeaderComponent },
 
 
 
