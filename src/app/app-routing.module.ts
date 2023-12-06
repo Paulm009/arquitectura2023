b13e0d4da2ebda4d6480b0c8a4f3150ad35b23c1
Huascar Nina <huascarnina@gmail.com>
2023-12-06 18:19:48 -0400

    [UPDATE] header, footer, login terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CarruselComponent } from './carrusel/carrusel.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 const routes: Routes = [
   { path: "carrusel", component: CarruselComponent },
   { path: "login", component: LoginComponent },
   { path: "header", component: HeaderComponent },
+  { path: "footer", component: FooterComponent },
+
 
 
 
