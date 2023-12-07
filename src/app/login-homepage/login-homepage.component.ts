4ff135277dfb4c5f28924f9eb66ab936243af615
Paulm009 <poolpos8@gmail.com>
2023-12-07 01:51:25 -0400

    integracion del carrusel

---
+++ src/app/login-homepage/login-homepage.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+import {  ViewChild, ElementRef } from '@angular/core';
+@Component({
+  selector: 'app-login-homepage',
+  templateUrl: './login-homepage.component.html',
+  styleUrls: ['./login-homepage.component.css']
+})
+export class LoginHomepageComponent implements OnInit {
+  @ViewChild('cardsContainer') cardsContainer: ElementRef;
+  cards = [
+    { title: 'Card 1', subtitle: 'Subtitle 1', content: 'holasi', image: '/assets/sliderimagens/ima1.jpg' },
+    { title: 'Card 1', subtitle: 'Subtitle 1', content: 'Content 1', image: '/assets/sliderimagens/ima1.jpg' },
+    { title: 'Card 1', subtitle: 'Subtitle 1', content: 'Content 1', image: '/assets/sliderimagens/ima2.jpg' },
+    { title: 'Card 1', subtitle: 'Subtitle 1', content: 'Content 1', image: '/assets/sliderimagens/ima3.jpg' },
+    { title: 'Card 1', subtitle: 'Subtitle 1', content: 'Content 1', image: '/assets/sliderimagens/ima4.jpg' },
+    { title: 'Card 1', subtitle: 'Subtitle 1', content: 'Content 1', image: '/assets/sliderimagens/ima5.jpg' },
+   
+    // ... Añade más tarjetas según sea necesario
+  ];
+
+  scrollLeft() {
+    this.cardsContainer.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
+  }
+
+  scrollRight() {
+    this.cardsContainer.nativeElement.scrollBy({ left: 300, behavior: 'smooth' });
+  }
+
+
+  formulario: FormGroup;
+  hide = true;
+
+  constructor(private formBuilder: FormBuilder,) {
+    this.formulario = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+
+  }
+
+}
